7953edb6216c2a63315712d11a8ea65ca0414730
thomas-bressel <tbressel@outlook.fr>
2026-03-27 13:11:25 +0100

    feat: remove middleware cors

--- internal/router/router.go
+++ internal/router/router.go
@@ -31,7 +31,6 @@ func Create(articlesHandler *handler.ArticlesHandler, trackHandler *handler.Trac
 	mux.Handle("GET /metrics", promhttp.Handler())
 
 	return pkg.ApplyMiddlewares(mux,
-		pkg.CORSMiddleware,
 		pkg.LogMiddleware,
 		pkg.RecoverMiddleware,
 		pkg.MetricsMiddleware,
